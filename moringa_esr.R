12646c512678460a88eb06df44a160bf2808e899
Yoel Gebrai <yoelgebrai@gmail.com>
2024-01-11 16:36:48 -0500

    R script

---
+++ moringa_esr.R
@@ -0,0 +1,20 @@
+#Install and load necessary packages
+
+
+#Define Variables
+
+
+#Descriptive statistics
+
+
+#OLS Regression
+
+
+#2SLS
+
+
+
+#2SLS continued
+
+
+#Hausman test for endogeneity of regressors
